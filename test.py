4f994de63d8d5d758b387a2853c998857a15f244
hakan280 <hkara419@gmail.com>
2015-04-14 10:03:02 +0300

    45

--- test.py
+++ test.py
@@ -3,62 +3,79 @@ from textblob.classifiers import NaiveBayesClassifier
 import codecs
 import json
 train = [
-    ('I love  this sandwich.', 'pos'),
-    ('This is an amazing place!', 'pos'),
-    ('I feel very good about these beers.', 'pos'),
-    ('This is my best work.', 'pos'),
-    ("What an awesome view", 'pos'),
-    ('I do not like this restaurant', 'neg'),
-    ('I am tired of this stuff.', 'neg'),
-    ("I can't deal with this", 'neg'),
-    ('He is my sworn enemy!', 'neg'),
-    ('My boss is horrible.', 'neg')
+	('I love  this sandwich.', 'pos'),
+	('This is an amazing place!', 'pos'),
+	('I feel very good about these beers.', 'pos'),
+	('This is my best work.', 'pos'),
+	("What an awesome view", 'pos'),
+	('I do not like this restaurant', 'neg'),
+	('I am tired of this stuff.', 'neg'),
+	("I can't deal with this", 'neg'),
+	('He is my sworn enemy!', 'neg'),
+	('My boss is horrible.', 'neg')
 ]
 test = [
-    ('The beer was good.', 'pos'),
-    ('I do not enjoy my job', 'neg'),
-    ("I ain't feeling dandy today.", 'neg'),
-    ("I feel amazing!", 'pos'),
-    ('Gary is a friend of mine.', 'pos'),
-    ("I can't believe I'm doing this.", 'neg')
-    ]
+	('The beer was good.', 'pos'),
+	('I do not enjoy my job', 'neg'),
+	("I ain't feeling dandy today.", 'neg'),
+	("I feel amazing!", 'pos'),
+	('Gary is a friend of mine.', 'pos'),
+	("I can't believe I'm doing this.", 'neg')
+	]
 
-counter=0
-listem=[]
-""" ------------------JSON PARSER-----------
-with open('../hepsi_v3/data.json') as data_file:    
-    data = json.load(data_file)
-print(type(data))
 
-#f = open('hepsi5', 'w')
-for i in data:
-    comment=(i["reviews"])
-    for c in comment:
-        #if c["star"]=="5":
 
-        listem.append((c["comment"].strip() , c["star"].decode("utf-8") ))    
-            #toFile=(c["star"].encode("utf-8") + "   " + c["comment"].encode("utf-8").strip() + "\n" )
-            #f.write(toFile)
-            #print toFile
-        counter+=1
-"""
+# ------------------JSON PARSER-----------
+def parse_json(json_path,txt_name):
+	#'../hepsi_v3/data.json'
+	with open(json_path) as data_file:    
+		data = json.load(data_file)
+		print(type(data))
+	counter=0
+	f = open(txt_name, 'w')
+	for i in data:
+		comment=(i["reviews"])
+		for c in comment:
+			if c["star"]=="5":
 
+				#listem.append((c["comment"].strip() , c["star"].decode("utf-8") ))    
+				toFile=(c["star"].encode("utf-8") + "   " + c["comment"].encode("utf-8").strip() + "\n" )
+				f.write(toFile)
+			
+				counter+=1
+
+parse_json('../hepsi_v3/data.json',"tablet_comments5")
 
 # .TXT PARSE 
-f = codecs.open("train_data/train_data", "r", encoding="utf-8")
-for line in f:
-    div=line.split()
-    star=div[0]
-    comment=div[1:]
-    comment=" ".join(comment)
-    
-    listem.append((comment.lower(), star )) 
+def parse_txt(path):
+	listem=[]
+	f = codecs.open(path, "r", encoding="utf-8")
+	for line in f:
+		div=line.split()
+		star=div[0]
+		comment=div[1:]
+		comment=" ".join(comment)
+		listem.append((comment.lower(), star )) 
+	return listem
+
+train_list=parse_txt("train_data/train_data")
+test_list=parse_txt("train_data/test_data")
+t45=parse_txt("train_data/45")
+print len(test_list)
+
+cl = NaiveBayesClassifier(train_list)
+cl2=NaiveBayesClassifier(t45)
+def find_star(comment):
+	star1=cl.classify(comment)
+
+	if star1=="5":
+		star2=cl2.classify(comment)
+		return star2
 
 
 
-print counter
-print listem[:10]
-cl = NaiveBayesClassifier(listem)
+"""
+
 
 print(cl.classify((u"Ürün çok kullanışlı, tavsiye ederim ")))  
 print(cl.classify((u"hemem bozuldu kalitesiz, aldığıma pişmanım")) )
@@ -67,4 +84,6 @@ print(cl.classify(U"kötü"))
 print(cl.classify((u"Müthiş, görüntü kalitesi mükemmel.")))  
 print(cl.classify((u"Verdiğiniz paraya değer tavsive ederim ")))  
 print(cl.classify((u"çok dandik, işe yaramaz"))) 
-print(cl.classify((u"kalitesiz")))  
+print(cl.classify((u"kalitesiz")))
+print(cl.classify((u"ürün kaliteli")))    
+"""
